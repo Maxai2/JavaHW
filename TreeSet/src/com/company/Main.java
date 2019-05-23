24e12d191800ac3f61d02a28583157f1c1dc256a
Махмудов Али Ильгар <Mahm_iz81@itstep.edu.az>
2019-05-23 12:04:53 +0400

    'person'

--- TreeSet/src/com/company/Main.java
+++ TreeSet/src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
-public class Main {
+import lombok.Data;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+
+    }
+}
+
+@Data
+class Student {
+    String name;
+    String surname;
+    double avg;
+
+    Student(String name, String surname, double avg) {
+        this.name = name;
+        this.surname = surname;
+        this.avg = avg;
+    }
+}
+
+interface Comparator<T> {
+    boolean compare(T a, T b);
+}
+
+class TreeSet<T> {
+
+
+
+    @Data
+    class Node<T> {
+        T value;
+
     }
 }
