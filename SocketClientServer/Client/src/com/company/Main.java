84350d47b5b87c9b0b3cb534bf1b0ab4478a1d17
Махмудов Али Ильгар <Mahm_iz81@itstep.edu.az>
2019-06-03 11:55:09 +0400

    'socketStart'

---
+++ SocketClientServer/Client/src/com/company/Main.java
@@ -0,0 +1,91 @@
+package com.company;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Main {
+
+    private static Socket clientSocket;
+    private static BufferedReader reader;
+    private static BufferedReader in;
+    private static BufferedWriter out;
+    private static boolean isWork = true;
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            try
+            {
+                clientSocket = new Socket("localhost", 27001);
+                reader = new BufferedReader(new InputStreamReader(System.in));
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                //
+                class ReadMsg extends Thread {
+                    @Override
+                    public void run() {
+
+                        String str;
+                        try {
+                            while (true) {
+                                str = in.readLine();
+                                if (str.equals("stop"))
+                                {
+                                    isWork = false;
+                                    break;
+                                }
+                                System.out.println(str);
+                            }
+                        }
+                        catch (IOException e) {}
+                    }
+                }
+
+                //
+                class WriteMsg extends Thread {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            String userWord;
+                            try
+                            {
+                                userWord = reader.readLine();
+                                if (userWord.equals("stop"))
+                                {
+                                    out.write("stop" + "\n");
+                                    isWork = false;
+                                    break;
+                                }
+                                else {
+                                    out.write(userWord + "\n");
+                                }
+                                out.flush();
+                            } catch (IOException e) {
+
+                            }
+
+                        }
+                    }
+                }
+
+                new ReadMsg().start();
+                new WriteMsg().start();
+
+                while(isWork){}
+
+            }
+            finally
+            {
+                System.out.println("Клиент был закрыт...");
+                clientSocket.close();
+                in.close();
+                out.close();
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+    }
+}
--- verseSort/src/com/company/Main.java
+++ verseSort/src/com/company/Main.java
@@ -1,75 +1,124 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
     static ArrayList<String> verse = new ArrayList<>();
+    static ArrayList<String> verseSorted = new ArrayList<>();
+    private static BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+    static boolean ready = false;
+    static boolean start = false;
+    static String sortInfo = "";
+
+//    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
+        newVerse();
 
         while (true) {
-            System.out.println("Paste the verse:");
-            while (true) {
-                String str = scan.nextLine();
-
-                if (str.equals("")) {
-                    break;
+            if (ready) {
+                System.out.println("-------------------------------------------");
+                System.out.println("Your text is sorted " + sortInfo);
+                System.out.print("Show?(y/n): ");
+                Character ans = null;
+                try {
+                    ans = (char)System.in.read();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                verse.add(str);
+
+                if (ans.equals('y')) {
+                    threadAns(verseSorted);
+                    ready = false;
+                }
             }
+
+//            System.out.println("-------------------------------------------");
+//            newVerse();
 
             System.out.println("\nChoose method of sort verse:");
             System.out.println("1) Quick sort");
             System.out.println("2) Bubble sort");
             System.out.print("Your choice(1/2): ");
-            byte ch = scan.nextByte();
+            int ch = 0;
+            try {
+                ch = Integer.parseInt(scan.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             System.out.println("\n1) Low to high");
             System.out.println("2) High to low");
             System.out.print("Your choice(1/2): ");
-            byte rev = scan.nextByte();
+            int rev = 0;
+            try {
+                rev = Integer.parseInt(scan.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             if (ch == 1) {
                 if (rev == 1) {
-                    new quickSortCl(verse.clone(), true, (verseNew)->{
-                        System.out.println("\n");
-                        for (String row : verseNew) {
-                            System.out.println(row);
-                        }
+                    Thread t = new quickSortCl(verse.clone(), true, (verseNew)->{
+                        sortInfo = "Quick sort | Low to high";
+                        verseSorted = verseNew;
+                        ready = true;
                     });
                 } else if (rev == 2) {
                     new quickSortCl(verse.clone(), false, (verseNew)->{
-                        System.out.println("\n");
-                        for (String row : verseNew) {
-                            System.out.println(row);
-                        }
+                        sortInfo = "Quick sort | High to low";
+                        verseSorted = verseNew;
+                        ready = true;
                     });
                 }
-                break;
             } else if (ch == 2) {
                 if (rev == 1) {
                     new bubbleSortCl(verse.clone(), true, (verseNew)->{
-                        System.out.println("\n");
-                        for (String row : verseNew) {
-                            System.out.println(row);
-                        }
+                        sortInfo = "Bubble sort | Low to high";
+                        verseSorted = verseNew;
+                        ready = true;
                     });
                 } else if (rev == 2) {
                     new bubbleSortCl(verse.clone(), false, (verseNew)->{
-                        System.out.println("\n");
-                        for (String row : verseNew) {
-                            System.out.println(row);
-                        }
+                        sortInfo = "Bubble sort | High to low";
+                        verseSorted = verseNew;
+                        ready = true;
                     });
                 }
-                break;
             } else {
                 System.out.println("Choose correct!");
+
             }
+        }
+    }
+
+    public static void newVerse() {
+        System.out.println("Paste the verse:");
+        verse.clear();
+        while (true) {
+
+            String str = null;
+            try {
+                str = scan.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (str.equals("")) {
+                break;
+            }
+            verse.add(str);
+        }
+    }
+
+    public static void threadAns(ArrayList<String> newVerse) {
+        System.out.println("\n");
+        for (String row : newVerse) {
+            System.out.println(row);
         }
     }
 }
@@ -123,7 +172,6 @@ class quickSortCl extends Thread {
         quickSort(firstEnt, cur, rev);
         quickSort(cur + 1, lastEnt, rev);
     }
-
 }
 
 class bubbleSortCl extends Thread {
